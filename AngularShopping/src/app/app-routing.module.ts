03b7e43e1079296f0d6bcc3252d8ad427d53960b
IVY <43027706+IVYSS@users.noreply.github.com>
2022-07-27 10:39:21 +0700

    test

--- AngularShopping/src/app/app-routing.module.ts
+++ AngularShopping/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { UserComponent } from './components/user/user.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '**',
+  redirectTo: '/home',
+  component: UserComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
